925ec240dc2f9b111e8d19cf650d43b21ff04f87
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2024-01-16 06:31:55 +0100

    Mostly working version

--- convert-all.py
+++ convert-all.py
@@ -1,6 +1,46 @@
 #!/usr/bin/env python
+
+import fileinput
+import os.path
+import re
 import subprocess
 
 with open("input/needed.txt") as needed:
-    for line in needed:
-        subprocess.call(["./convert.sh" ,line ])
+    lines = [line.rstrip() for line in needed]
+
+lines.sort()
+
+size_regex = re.compile('width="(\\d+)" height="(\\d+)"')
+
+for line in lines:
+    replaced = line.replace(" ", "-").replace("+", "-")
+    svg_filename = f"output/{replaced}.svg"
+    if os.path.isfile(svg_filename):
+        with open(svg_filename) as svg:
+            header = svg.readline()
+            match = size_regex.search(header)
+
+            (width, height) = match.groups()
+            width = int(width)
+            height = int(height)
+    else:
+        subprocess.call(["./convert.sh", line, svg_filename])
+
+        for svgline in fileinput.input(svg_filename, inplace=True):
+            match = size_regex.search(svgline)
+            if match:
+                (width, height) = match.groups()
+                width = int(width) - 20
+                height = int(height) - 20
+                print(
+                    f'<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="{width}" height="{height}" viewBox="10 10 {width} {height}">',
+                )
+            else:
+                print(svgline, end="")
+
+    print(
+        replaced,
+        f'\n<ruby><div style="display:inline-block;background-color:currentColor;width:{width}px;height:{height}px;mask:url(https://toki-pona-svg.netlify.app/{replaced}.svg)"></div> <rt>{replaced.replace("-"," ")}</rt></ruby>\n',
+    )
+
+# <div style="background-color: currentColor; height: 18px; mask: url(http://localhost:8080/output.svg);width: 154px;display: inline-block;"></div>
--- convert.sh
+++ convert.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env nix-shell
 #! nix-shell -i bash -p pango -p graphicsmagick
-pango-view --output="output/$1.svg" --font="Linja Pona" -q --text "$1"
-npx svgo --multipass --pretty -p2 "output/$1.svg"
+set -e
+mkdir -p output
+chronic pango-view --output="$2" --font="Linja Pona" -q --text "$1"
+chronic npx svgo --multipass --pretty -p2 "$2"
